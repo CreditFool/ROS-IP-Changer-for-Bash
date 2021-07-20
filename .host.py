d3bb7cf8e3bd03ad4c3a65f5ed62bf36d13887e0
Ichlasul Bulqiah <bichlasulbulqiah@gmail.com>
2021-07-20 21:54:26 +0700

    200721 - Menambahkan fungsi changeHost

--- .host.py
+++ .host.py
@@ -4,9 +4,15 @@ from os import close, read
 import subprocess
 from typing import final
 import shutil
+import socket
 
 bashrc_path = "/home/creditfool/kodingan/Project/host swithcer/dummy_bash"
 bkp_path = bashrc_path+".bkp"
+
+local = "localhost"
+computerIP = "192.168.0.102"
+host = socket.gethostbyname(socket.gethostname())
+print(host)
 
 
 def makeBackup(source: str, target: str):
@@ -24,19 +30,27 @@ def makeBackup(source: str, target: str):
 
 
 makeBackup(bashrc_path, bkp_path)
-try:
-    with open(bashrc_path, 'r') as reader, open("outdummy", "w") as writer:
-        new_content = ''
-        for line in reader:
-            if "export ROS_MASTER_URI" in line and line[0] != '#':
-                print(line)
-            #new_content += line
 
-        writer.write(new_content)
 
-finally:
-    reader.close()
-    writer.close()
+def changeHost(master='', hostname=''):
+    try:
+        with open(bashrc_path, 'r') as reader, open("outdummy", "w") as writer:
+            new_content = ''
+            for line in reader:
+                line_strip = line.strip()
+                if "export ROS_MASTER_URI" in line and line[0] != '#':
+                    line_strip = "export ROS_MASTER_URI=http://"+local+":11311"
+
+                if "export ROS_HOSTNAME" in line and line[0] != '#':
+                    line_strip = "export ROS_HOSTNAME="+local
+
+                new_content += line_strip+"\n"
+
+            writer.write(new_content)
+
+    finally:
+        reader.close()
+        writer.close()
 
 # subprocess.run(["clear"])
 # subprocess.run(["bash"])
